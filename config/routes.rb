953097dabd574aca8abf4e94a27fd63663d624b1
Dan Amar <danamar2001@gmail.com>
2022-03-31 18:50:15 -0400

    created a pages controller and added routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'public#main'
+  root to: 'pages#main'
+  get 'about',           to: 'pages#about',      as: :about
+  get 'contact',         to: 'pages#contact',    as: :contact
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
