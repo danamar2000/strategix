c61168291b74fe9ea4897a8fbad9fad31adc4b26
dan amar <danamar2001@gmail.com>
2022-04-04 14:40:14 -0400

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,16 @@
 
 ActiveRecord::Schema.define(version: 2022_04_01_194327) do
 
-  create_table "accounts", force: :cascade do |t|
+  create_table "schools", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.string "first_name", default: "", null: false
-    t.string "last_name", default: "", null: false
+    t.string "first_last", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "url"
@@ -30,32 +37,6 @@ ActiveRecord::Schema.define(version: 2022_04_01_194327) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_accounts_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
-  end
-
-  create_table "properties", force: :cascade do |t|
-    t.integer "account_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["account_id"], name: "index_properties_on_account_id"
-  end
-
-  create_table "schools", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
